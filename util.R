b473903413f815dc6298ed20f02d229fba0b3153
Huey Kwik <huey.kwik@gmail.com>
2016-06-08 13:55:51 -0700

    Adds random partitioning.

--- util.R
+++ util.R
@@ -2,3 +2,9 @@ trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 
 # Freedman-Diaconis rule for calculating bin width.
 bw = function(x) diff(range(x)) / (2 * IQR(x) / length(x)^(1/3))
+
+mode = function(x) {
+  ux = unique(x)
+  tab = tabulate(match(x, ux))
+  ux[tab == max(tab)]
+}
